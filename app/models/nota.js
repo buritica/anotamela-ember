a2f0b29a66b06b5d2b7f05d35b6fc60cd2f9d0f3
Juan Pablo Buritica <buritica@gmail.com>
2014-08-24 18:57:59 -0400

    [fix] conflict with `content` property in model

--- app/models/nota.js
+++ app/models/nota.js
@@ -3,6 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   title: DS.attr('string'),
   description: DS.attr('string'),
-  type: DS.attr('string'),
-  content: DS.attr('string')
+  language: DS.attr('string'),
+  body: DS.attr('string')
 });
