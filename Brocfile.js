c07a44fbd6f79214f12f9596e0c03496ff65bc49
Juan Pablo Buritica <buritica@gmail.com>
2014-08-24 18:03:08 -0400

    [install] foundation

--- Brocfile.js
+++ Brocfile.js
@@ -17,4 +17,7 @@ var app = new EmberApp();
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
 
+app.import('vendor/foundation/css/normalize.css')
+app.import('vendor/foundation/css/foundation.css')
+
 module.exports = app.toTree();
