c2a048164a4fbcae6dc6ba15e78998ac997e71f6
Juan Pablo Buritica <buritica@gmail.com>
2014-08-24 20:02:38 -0400

    [add] ace editor textarea component

--- app/components/ace-editor.js
+++ app/components/ace-editor.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  didInsertElement: function() {
+    this.editor = window.ace.edit(this.get('element'));
+    this.get('aceInit')(this.editor);
+    this.editor.on('change', function(){
+      this.set('value', this.editor.getSession().getValue());
+    }.bind(this));
+  },
+  valueChanged: function () {
+    if (!this.get('value')) {
+      this.editor.getSession().setValue('');
+    }
+    else if (this.editor.getSession().getValue() !== this.get('value')) {
+      this.editor.getSession().setValue(this.get('value'));
+    }
+  }.observes('value')
 });
--- app/controllers/nueva.js
+++ app/controllers/nueva.js
@@ -6,5 +6,11 @@ export default Ember.ObjectController.extend({
       console.log('submit');
       return false;
     }
+  },
+   aceInit: function(editor) {
+    editor.setHighlightActiveLine(false);
+    editor.setShowPrintMargin(false);
+    editor.getSession().setTabSize(2);
+    editor.getSession().setMode("ace/mode/javascript");
   }
 });
