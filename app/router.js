41873dd2e6ff01ffc0110d29bb290fbc3426625a
Juan Pablo Buritica <buritica@gmail.com>
2014-08-25 18:27:21 -0400

    [add] notas as main route

--- app/router.js
+++ app/router.js
@@ -5,7 +5,7 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.resource('notas', function() {
+  this.resource('notas', {path: '/'}, function() {
     this.route('nueva');
   });
 
