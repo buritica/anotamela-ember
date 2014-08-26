a8a7ca6a7c0467136743ba682acf025cf9c8cdaa
Juan Pablo Buritica <buritica@gmail.com>
2014-08-25 23:29:51 -0500

    [rename] language to type

--- app/models/nota.js
+++ app/models/nota.js
@@ -8,6 +8,6 @@ inflector.singular(/nota/, 'nota');
 export default DS.Model.extend({
   title: DS.attr('string'),
   description: DS.attr('string'),
-  language: DS.attr('string'),
+  type: DS.attr('string'),
   body: DS.attr('string')
 });
--- app/routes/notas/nueva.js
+++ app/routes/notas/nueva.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function() {
     return this.store.createRecord('nota', {
-      language: 'js'
+      type: 'js'
     });
   }
 });
